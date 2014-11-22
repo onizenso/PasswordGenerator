792c59559c43cba1c6a455ae1792042c168823c1
onizenso <onicrypt@gmail.com>
2014-11-21 22:33:53 -0800

    Rearranged methods under class
    Added most of the methods under the class to enhance
    the design of the generator. Reduced ammount of repeated
    code to embrace DRY methodology.

--- pw-gen.rb
+++ pw-gen.rb
@@ -1,61 +1,74 @@
 require 'securerandom'
 require 'pry'
 
-def rand_num(n)
-  SecureRandom.random_number(n)
-end
+class PasswordGenerator
+  attr_accessor :file_name 
+  def initialize(file_name)
+    @file_name = file_name
+  end
 
-def build_word_list(fn)
-  list = []
-  if File.readable? fn
-    File.open(fn,"r"){|f|
-      f.each_line {|l|
-        list.push l.strip if l.length >= 5 && l.length <= 15
+  def rand_num(n)
+    SecureRandom.random_number(n)
+  end
+  
+  def build_word_list
+    list = []
+    if File.readable? @file_name 
+      File.open(@file_name,"r"){|f|
+        f.each_line {|l|
+          list.push l.strip if l.length >= 5 && l.length <= 15
+        }
       }
+    end
+    return list
+  end
+  
+  def list_select(list, cnt=5)
+    nl = []
+    cnt.times do
+      rn = rand_num(list.length - 1)
+      nl.push list[rn]
+    end
+    return nl
+  end
+  
+  def special_number_select(spec_string, cnt)
+    sp = []
+    sp = list_select(spec_string.chars, cnt)
+    return sp.join
+  end
+  
+  def insert_separator(list, spec_string)
+    new_list = []
+    spec_string = spec_string.chars
+    list.collect {|bw| 
+      sp = list_select(spec_string)
+      bw.insert(rand_num(bw.length - 1), sp.join)
     }
+    return list 
   end
-  return list
-end
 
-def list_select(list)
-  rn = rand_num(list.length - 1)
-  return list[rn]
-end
-
-def special_number_select(sn)
-  sp = []
-  3.times do
-    sp.push list_select(sn)
+  def build_spec_num
+    special_chars = "~`!@\#$%^*()_-+={}[]:\";'<>?,./\\|"
+    numbers = (0..9).to_a.join
+    special_chars.concat( numbers )
+    return special_chars
   end
-  return sp.join
 end
 
-
-def insert_separator(list, schars)
-  new_list = []
-  list.each {|bw|
-    sp = special_number_select(schars) 
-    c = bw.chars
-    c.insert( rand_num(c.length - 1), sp )
-    new_list.push c.join
-  }
-  return new_list
-end
-
-special_chars = "~`!@\#$%^*(()_-+={}[]:\";'<>?,./\\|".chars
-numbers = (0..9).to_a
-spec_num = special_chars.join.concat(numbers.join)
-
+# == Password Generator for Modified XKCD
+# Set WORD_LIST environment variable to 
+# desired dictionary of words
 file_name = ENV['WORD_LIST'].to_s 
-dict_list = build_word_list(file_name)
 
-base_words = []
-3.times do
-  base_words.push list_select(dict_list)
-end
+pass_gen = PasswordGenerator.new(file_name)
+dict_list = pass_gen.build_word_list
 
-mod_words = insert_separator(base_words, spec_num)
-password = "#{mod_words[1]}#{mod_words[2]}#{mod_words[0]}"
+base_words = pass_gen.list_select(dict_list, 3)
+spec_num = pass_gen.build_spec_num
+
+modified_words = pass_gen.insert_separator(base_words, spec_num)
+password = "#{modified_words[1]}#{modified_words[2]}#{modified_words[0]}"
 
 puts password
-#binding.pry
+binding.pry
